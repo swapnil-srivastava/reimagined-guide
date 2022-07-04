2cbac722856debddcc101db8d215d905eecbf467
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-04 23:42:39 +0200

    adding the dark mod and another route for tech stack

---
+++ components/TechBox.tsx
@@ -0,0 +1,9 @@
+function TechBox({techStackName}) {
+    return (
+        <div className="px-5 py-2 m-1 text-1xl ">
+            {techStackName}
+        </div>
+    );
+}
+
+export default TechBox;
---
+++ pages/technology.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import TechBox from "../components/TechBox";
+
+const initialTechStackState = [
+    {
+        techName: "JavaScript",
+    },
+    {
+        techName: "TypeScript",
+    },
+    {
+        techName: "CSS",
+    },
+    {
+        techName: "CSS4",
+    },
+    {
+        techName: "HTML",
+    },
+    {
+        techName: "HTML5",
+    },
+    {
+        techName: "ES6/7",
+    },
+    {
+        techName: "Redux",
+    },
+    {
+        techName: "ngRX",
+    },
+    {
+        techName: "Angular",
+    },
+]
+
+export default function Technology() {
+  const [techStack, setTechStack] = useState(initialTechStackState);
+
+  function techStackCheck(): void {
+    console.log('================techStack====================');
+    console.log(techStack);
+    console.log('=================techStack===================');
+  }
+
+  return (
+    <>
+        <div className="flex py-10 px-10 flex-wrap">
+        {techStack && techStack.map(({techName}) => <TechBox key={techName} techStackName={techName} />)}
+        </div>
+        <button onClick={() => techStackCheck()}>Tech Stack</button>
+    </>
+  );
+}
+
+
