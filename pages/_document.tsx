364fcaf3e48f4f336c109367bb4a69401325a0a9
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-06 00:59:27 +0200

    adding the background of white and text color of black

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head />
-            <body className="dark:bg-gray-800">
+            <body className="bg-blog-white dark:bg-gray-800">
                 <Main />
                 <NextScript />
             </body>
