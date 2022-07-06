8bb87d1a8e482120db435089341c1e9b6e402690
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-07 00:37:40 +0200

    adding serverless api and tech stack typescript interface

---
+++ pages/api/techstack.ts
@@ -0,0 +1,254 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+
+    const initialTechStackState = [
+        {
+            techName: "#JavaScript",
+            colorTechStack: "text-blog-white dark:text-blog-white bg-persian-blue-500 hover:bg-persian-blue-600 " 
+        },
+        {
+            techName: "#TypeScript",
+            colorTechStack: "text-blog-white dark:text-blog-white bg-dark-blue-500 hover:bg-dark-blue-600" 
+        },
+        {
+            techName: "#CSS",
+            colorTechStack: "dark:text-blog-black bg-caribbean-green-500 hover:bg-caribbean-green-600" 
+        },
+        {
+            techName: "#CSS4",
+            colorTechStack: "" 
+        },
+        {
+            techName: "#HTML",
+        },
+        {
+            techName: "#HTML5",
+        },
+        {
+            techName: "#ES6/7",
+        },
+        {
+            techName: "#Redux",
+        },
+        {
+            techName: "#ngRX",
+        },
+        {
+            techName: "#Angular",
+        },
+        {
+            techName: "#JavaScript",
+        },
+        {
+            techName: "#TypeScript",
+        },
+        {
+            techName: "#CSS",
+        },
+        {
+            techName: "#CSS4",
+        },
+        {
+            techName: "#HTML",
+        },
+        {
+            techName: "#HTML5",
+        },
+        {
+            techName: "#ES6/7",
+        },
+        {
+            techName: "#Redux",
+        },
+        {
+            techName: "#ngRX",
+        },
+        {
+            techName: "#Angular",
+        },
+        {
+            techName: "#JavaScript",
+        },
+        {
+            techName: "#TypeScript",
+        },
+        {
+            techName: "#CSS",
+        },
+        {
+            techName: "#CSS4",
+        },
+        {
+            techName: "#HTML",
+        },
+        {
+            techName: "#HTML5",
+        },
+        {
+            techName: "#ES6/7",
+        },
+        {
+            techName: "#Redux",
+        },
+        {
+            techName: "#ngRX",
+        },
+        {
+            techName: "#Angular",
+        },
+        {
+            techName: "#JavaScript",
+        },
+        {
+            techName: "#TypeScript",
+        },
+        {
+            techName: "#CSS",
+        },
+        {
+            techName: "#CSS4",
+        },
+        {
+            techName: "#HTML",
+        },
+        {
+            techName: "#HTML5",
+        },
+        {
+            techName: "#ES6/7",
+        },
+        {
+            techName: "#Redux",
+        },
+        {
+            techName: "#ngRX",
+        },
+        {
+            techName: "#Angular",
+        },
+        {
+            techName: "#JavaScript",
+        },
+        {
+            techName: "#TypeScript",
+        },
+        {
+            techName: "#CSS",
+        },
+        {
+            techName: "#CSS4",
+        },
+        {
+            techName: "#HTML",
+        },
+        {
+            techName: "#HTML5",
+        },
+        {
+            techName: "#ES6/7",
+        },
+        {
+            techName: "#Redux",
+        },
+        {
+            techName: "#ngRX",
+        },
+        {
+            techName: "#Angular",
+        },
+        {
+            techName: "#JavaScript",
+        },
+        {
+            techName: "#TypeScript",
+        },
+        {
+            techName: "#CSS",
+        },
+        {
+            techName: "#CSS4",
+        },
+        {
+            techName: "#HTML",
+        },
+        {
+            techName: "#HTML5",
+        },
+        {
+            techName: "#ES6/7",
+        },
+        {
+            techName: "#Redux",
+        },
+        {
+            techName: "#ngRX",
+        },
+        {
+            techName: "#Angular",
+        },
+        {
+            techName: "#JavaScript",
+        },
+        {
+            techName: "#TypeScript",
+        },
+        {
+            techName: "#CSS",
+        },
+        {
+            techName: "#CSS4",
+        },
+        {
+            techName: "#HTML",
+        },
+        {
+            techName: "#HTML5",
+        },
+        {
+            techName: "#ES6/7",
+        },
+        {
+            techName: "#Redux",
+        },
+        {
+            techName: "#ngRX",
+        },
+        {
+            techName: "#Angular",
+        },
+        {
+            techName: "#JavaScript",
+        },
+        {
+            techName: "#TypeScript",
+        },
+        {
+            techName: "#CSS",
+        },
+        {
+            techName: "#CSS4",
+        },
+        {
+            techName: "#HTML",
+        },
+        {
+            techName: "#HTML5",
+        },
+        {
+            techName: "#ES6/7",
+        },
+        {
+            techName: "#Redux",
+        },
+        {
+            techName: "#ngRX",
+        },
+        {
+            techName: "#Angular",
+        }
+    ];
+
+    res.status(200).json({ techStack: initialTechStackState })
+  }
+  
--- pages/technology.tsx
+++ pages/technology.tsx
@@ -1,270 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Metatags from "../components/Metatags";
 import TechBox from "../components/TechBox";
+import * as axios from "axios";
 
-const initialTechStackState = [
-    {
-        techName: "#JavaScript",
-        colorTechStack: "text-blog-white dark:text-blog-white bg-persian-blue-500 hover:bg-persian-blue-600 " 
-    },
-    {
-        techName: "#TypeScript",
-        colorTechStack: "text-blog-white dark:text-blog-white bg-dark-blue-500 hover:bg-dark-blue-600" 
-    },
-    {
-        techName: "#CSS",
-        colorTechStack: "dark:text-blog-black bg-caribbean-green-500 hover:bg-caribbean-green-600" 
-    },
-    {
-        techName: "#CSS4",
-        colorTechStack: "" 
-    },
-    {
-        techName: "#HTML",
-    },
-    {
-        techName: "#HTML5",
-    },
-    {
-        techName: "#ES6/7",
-    },
-    {
-        techName: "#Redux",
-    },
-    {
-        techName: "#ngRX",
-    },
-    {
-        techName: "#Angular",
-    },
-    {
-        techName: "#JavaScript",
-    },
-    {
-        techName: "#TypeScript",
-    },
-    {
-        techName: "#CSS",
-    },
-    {
-        techName: "#CSS4",
-    },
-    {
-        techName: "#HTML",
-    },
-    {
-        techName: "#HTML5",
-    },
-    {
-        techName: "#ES6/7",
-    },
-    {
-        techName: "#Redux",
-    },
-    {
-        techName: "#ngRX",
-    },
-    {
-        techName: "#Angular",
-    },
-    {
-        techName: "#JavaScript",
-    },
-    {
-        techName: "#TypeScript",
-    },
-    {
-        techName: "#CSS",
-    },
-    {
-        techName: "#CSS4",
-    },
-    {
-        techName: "#HTML",
-    },
-    {
-        techName: "#HTML5",
-    },
-    {
-        techName: "#ES6/7",
-    },
-    {
-        techName: "#Redux",
-    },
-    {
-        techName: "#ngRX",
-    },
-    {
-        techName: "#Angular",
-    },
-    {
-        techName: "#JavaScript",
-    },
-    {
-        techName: "#TypeScript",
-    },
-    {
-        techName: "#CSS",
-    },
-    {
-        techName: "#CSS4",
-    },
-    {
-        techName: "#HTML",
-    },
-    {
-        techName: "#HTML5",
-    },
-    {
-        techName: "#ES6/7",
-    },
-    {
-        techName: "#Redux",
-    },
-    {
-        techName: "#ngRX",
-    },
-    {
-        techName: "#Angular",
-    },
-    {
-        techName: "#JavaScript",
-    },
-    {
-        techName: "#TypeScript",
-    },
-    {
-        techName: "#CSS",
-    },
-    {
-        techName: "#CSS4",
-    },
-    {
-        techName: "#HTML",
-    },
-    {
-        techName: "#HTML5",
-    },
-    {
-        techName: "#ES6/7",
-    },
-    {
-        techName: "#Redux",
-    },
-    {
-        techName: "#ngRX",
-    },
-    {
-        techName: "#Angular",
-    },
-    {
-        techName: "#JavaScript",
-    },
-    {
-        techName: "#TypeScript",
-    },
-    {
-        techName: "#CSS",
-    },
-    {
-        techName: "#CSS4",
-    },
-    {
-        techName: "#HTML",
-    },
-    {
-        techName: "#HTML5",
-    },
-    {
-        techName: "#ES6/7",
-    },
-    {
-        techName: "#Redux",
-    },
-    {
-        techName: "#ngRX",
-    },
-    {
-        techName: "#Angular",
-    },
-    {
-        techName: "#JavaScript",
-    },
-    {
-        techName: "#TypeScript",
-    },
-    {
-        techName: "#CSS",
-    },
-    {
-        techName: "#CSS4",
-    },
-    {
-        techName: "#HTML",
-    },
-    {
-        techName: "#HTML5",
-    },
-    {
-        techName: "#ES6/7",
-    },
-    {
-        techName: "#Redux",
-    },
-    {
-        techName: "#ngRX",
-    },
-    {
-        techName: "#Angular",
-    },
-    {
-        techName: "#JavaScript",
-    },
-    {
-        techName: "#TypeScript",
-    },
-    {
-        techName: "#CSS",
-    },
-    {
-        techName: "#CSS4",
-    },
-    {
-        techName: "#HTML",
-    },
-    {
-        techName: "#HTML5",
-    },
-    {
-        techName: "#ES6/7",
-    },
-    {
-        techName: "#Redux",
-    },
-    {
-        techName: "#ngRX",
-    },
-    {
-        techName: "#Angular",
-    }
-]
+type TechStack = {
+    techName: number;
+    colorTechStack: string;
+};
 
+type TechStackArray = { 
+    techStack : TechStack[] 
+}
+
+type GetTechStackResponse = {
+    data: TechStackArray[];
+};
+  
 export default function Technology() {
-  const [techStack, setTechStack] = useState(initialTechStackState);
+  const [techStack, setTechStack] = useState<TechStack[]>();
 
-  function techStackCheck(): void {
-    console.log('================techStack====================');
-    console.log(techStack);
-    console.log('=================techStack===================');
+  useEffect(() => {
+    getTechStack();
+  },[])
+
+  async function getTechStack() {
+
+    try {
+      // 👇️ const data: GetTechStackResponse
+      const { data, status } = await axios.get<GetTechStackResponse>(
+        '/api/techstack',
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+      );
+  
+      setTechStack(data.techStack);
+  
+      return data;
+
+    } catch (error) {
+      if (new axios.AxiosError(error)) {
+        console.log('error message: ', error.message);
+        return error.message;
+      } else {
+        console.log('unexpected error: ', error);
+        return 'An unexpected error occurred';
+      }
+    }
   }
+  
 
   return (
     <>
         <Metatags description={`Technology stack that I am fluent in`}/>
         <div className="flex py-10 px-10 flex-wrap">
-        {techStack && techStack.map(({techName, colorTechStack}) => <TechBox key={techName} techStackName={techName} techStackColor={colorTechStack}/>)}
+            {techStack && techStack.map(({techName, colorTechStack}) => <TechBox key={techName} techStackName={techName} techStackColor={colorTechStack}/>)}
         </div>
-        <button onClick={() => techStackCheck()}>Tech Stack</button>
     </>
   );
 }
