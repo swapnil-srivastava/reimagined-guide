93f233b856119b0c98cf08a5b126032609d75610
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-06 18:09:08 +0200

    adding the edit icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,7 +29,7 @@ function Navbar() {
     <nav className='navbar md:pl-4 dark:text-blog-white dark:bg-fun-blue-500'>
         <ul className='flex items-center justify-around md:justify-between'>
             <div className='flex'>
-                <li>
+                <li className='pl-2 md:pl-0'>
                     <Link href="/">
                         <button className='bg-hit-pink-500 text-blog-black'>FEED</button>
                     </Link>
@@ -57,10 +57,15 @@ function Navbar() {
                     <>  
                         <li>
                             <Link href="/admin">
-                                <button className='bg-hit-pink-500 text-blog-black'>Write Posts</button>
+                                <button className='bg-hit-pink-500 text-blog-black'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                        <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
+                                    </svg> 
+                                </button>
                             </Link>
                         </li>
-                        <li className='pr-3'>
+                        <li className='pr-0 md:pr-3'>
                             <Link href={`/${username}`}>
                                 <img src={user?.photoURL} alt="" />
                             </Link>
