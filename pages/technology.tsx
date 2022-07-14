ed212db52e0a013f8ebc7a12fe566d0a1ff598dc
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-14 14:46:00 +0200

    adding the bleedtech configuration

--- pages/technology.tsx
+++ pages/technology.tsx
@@ -16,10 +16,12 @@ type TechStackResponse = {
   
 export default function Technology() {
   const [techStackState, setTechStackState] = useState<TechStack[]>();
+  const [bleedingTechState, setBleedingTechState] = useState<TechStack[]>();
 
   useEffect(() => {
     // getTechStack();
     getTechStackFirebase();
+    getBleedingTechFirebase();
   },[])
 
   async function getTechStack() {
@@ -52,7 +54,7 @@ export default function Technology() {
     }
   }
   
-  async function getTechStackFirebase() {
+  async function getBleedingTechFirebase() {
 
     try {
       // 👇️ const data: GetTechStackResponse
@@ -79,11 +81,42 @@ export default function Technology() {
     }
   }
 
+  async function getTechStackFirebase() {
+
+    try {
+      // 👇️ const data: GetTechStackResponse
+      const q = query(collection(firestore, "leadingtech"));
+      const querySnapshot = await getDocs(q);
+
+      let tempDoc: TechStack[] = []
+
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        tempDoc = [...tempDoc, 
+          {
+            techName: doc.data().name,
+            colorTechStack: doc.data().techColor
+          }];
+      });
+
+      setBleedingTechState(tempDoc);
+      return tempDoc;
+    } catch (error) {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+
   return (
     <>
         <Metatags description={`Technology stack that I am fluent in`}/>
         <div className="flex py-10 px-10 flex-wrap">
             {techStackState && techStackState.map(({techName, colorTechStack}) => <TechBox key={techName} techStackName={techName} techStackColor={colorTechStack}/>)}
+        </div>
+
+        <div className="px-10 text-2xl font-extralight dark:text-blog-white">Bleeding Tech</div>
+        <div className="flex py-10 px-10 flex-wrap">
+            {bleedingTechState && bleedingTechState.map(({techName, colorTechStack}) => <TechBox key={techName} techStackName={techName} techStackColor={colorTechStack}/>)}
         </div>
     </>
   );
