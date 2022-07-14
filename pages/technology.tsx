5ac29f98061d3fed3817595289e0fd9d66512c7a
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-14 14:52:28 +0200

    adding the title and padding changes

--- pages/technology.tsx
+++ pages/technology.tsx
@@ -110,6 +110,8 @@ export default function Technology() {
   return (
     <>
         <Metatags description={`Technology stack that I am fluent in`}/>
+        <div className="px-10 text-2xl font-extralight dark:text-blog-white">Tech Stack</div>
+
         <div className="flex py-10 px-10 flex-wrap">
             {techStackState && techStackState.map(({techName, colorTechStack}) => <TechBox key={techName} techStackName={techName} techStackColor={colorTechStack}/>)}
         </div>
