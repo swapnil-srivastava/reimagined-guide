6c4986f053dbf17613ac852c5e231be70493b0b1
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-15 02:57:22 +0200

    adding three options for cards

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Link from "next/link";
-import { auth } from "../lib/firebase";
+import Image from "next/image";
 import { PencilAltIcon, HeartIcon } from "@heroicons/react/solid";
+import moment from "moment";
+
+// Auth
+import { auth } from "../lib/firebase";
 
 export function PostFeed({ posts, admin = false }) {
   return posts
@@ -44,24 +48,18 @@ function PostItem({ post, admin = false }) {
             "
       >
         <div className="basis-2/3">
-          
           <Link href={`/${post.username}`}>
-            <a className="text-md">
-             By @{post.username}
-            </a>
+            <a className="text-md">By @{post.username}</a>
           </Link>
 
           <div className="flex flex-wrap font-light gap-x-1">
             <Link href={`/${post.username}/${post.slug}`}>
-              <a>
-                {post.title}
-              </a>
+              <a>{post.title}</a>
             </Link>
 
             {/* <span className="font-light">({wordCount} words</span>
             <span className="font-light">{minutesToRead} min read)</span> */}
           </div>
-
 
           <div className="font-extralight">{contentTrimmed}</div>
 
@@ -81,8 +79,9 @@ function PostItem({ post, admin = false }) {
                     className="
                     bg-hit-pink-500 text-blog-black
                     dark:text-blog-black w-[calc(4rem_*_0.5)] h-[calc(4rem_*_0.5)] p-1 m-0.5 rounded-full flex items-center justify-center transition-filter duration-500 hover:filter hover:brightness-125
-                    ">
-                    <PencilAltIcon className="h-5 w-5"/>
+                    "
+                  >
+                    <PencilAltIcon className="h-5 w-5" />
                   </button>
                 </Link>
               </>
@@ -94,7 +93,7 @@ function PostItem({ post, admin = false }) {
           "
             >
               <div>{post.heartCount || 0}</div>
-              <HeartIcon className="h-5 w-5"/>
+              <HeartIcon className="h-5 w-5" />
             </div>
           </div>
 
@@ -103,6 +102,195 @@ function PostItem({ post, admin = false }) {
           ) : (
             <p className="text-danger self-end">Unpublished</p>
           )}
+        </div>
+      </div>
+
+      {/* New Design Option 1 */}
+      <div
+        className="p-3 my-4 mx-4 
+                  bg-blog-white 
+                  dark:bg-fun-blue-600 dark:text-blog-white
+                  rounded-lg
+                  drop-shadow-lg
+                  hover:drop-shadow-xl"
+      >
+        <div>
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <div className="text-3xl font-light">Title</div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="flex">
+                <div
+                  className="flex items-center justify-center
+                        bg-fun-blue-300 text-blog-black
+                        dark:text-blog-black w-[calc(4rem_*_0.5)] h-[calc(4rem_*_0.5)] p-1 m-0.5 rounded-full transition-filter duration-500 hover:filter hover:brightness-125
+                        "
+                >
+                  <div>{post.heartCount || 0}</div>
+                  <HeartIcon className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div className="flex items-center gap-x-2">
+            <Link href={`/${post.username}`}>
+              {post?.photoURL && post?.photoURL ? (
+                <div
+                  className="w-[calc(5rem_*_0.5)] h-[calc(5rem_*_0.5)] 
+                                  rounded-full cursor-pointer flex items-center 
+                                  overflow-hidden"
+                >
+                  <Image width={200} height={200} src={post.photoURL} alt="" />
+                </div>
+              ) : (
+                <div className="text-xs font-thin">{` ${post.username}`}</div>
+              )}
+            </Link>
+            <div className="">
+              <div className="flex gap-x-2">
+                <div className="font-extralight text-xs">9 Min Read</div>
+                <div className="font-extralight text-xs">45 Words</div>
+              </div>
+              <div className="font-extralight text-xs">
+                {`${moment(post.createdAt).format("DD MMM YYYY hh:mm a")}`}
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between">
+            <div>Post</div>
+            <div className="flex">
+              {post.published ? (
+                <p className="text-success self-end">Live</p>
+              ) : (
+                <p className="text-danger self-end">Unpublished</p>
+              )}
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      {/* New Design Option 2 */}
+      <div className="p-3 my-4 mx-4 
+                  bg-blog-white 
+                  dark:bg-fun-blue-600 dark:text-blog-white
+                  rounded-lg
+                  drop-shadow-lg
+                  hover:drop-shadow-xl">
+        <div>
+          <div className="flex justify-between">
+            <div className="flex items-center">
+            <div className="flex items-center gap-x-2">
+            <Link href={`/${post.username}`}>
+              {post?.photoURL && post?.photoURL ? (
+                <div
+                  className="w-[calc(5rem_*_0.5)] h-[calc(5rem_*_0.5)] 
+                                  rounded-full cursor-pointer flex items-center 
+                                  overflow-hidden"
+                >
+                  <Image width={200} height={200} src={post.photoURL} alt="" />
+                </div>
+              ) : (
+                <div className="text-xs font-thin">{` ${post.username}`}</div>
+              )}
+            </Link>
+            <div className="">
+              <div className="flex gap-x-2">
+                <div className="font-extralight text-xs">9 Min Read</div>
+                <div className="font-extralight text-xs">45 Words</div>
+              </div>
+              <div className="font-extralight text-xs">
+                {`${moment(post.createdAt).format("DD MMM YYYY hh:mm a")}`}
+              </div>
+            </div>
+          </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="flex">
+                <div
+                  className="flex items-center justify-center
+                        bg-fun-blue-300 text-blog-black
+                        dark:text-blog-black w-[calc(4rem_*_0.5)] h-[calc(4rem_*_0.5)] p-1 m-0.5 rounded-full transition-filter duration-500 hover:filter hover:brightness-125
+                        "
+                >
+                  <div>{post.heartCount || 0}</div>
+                  <HeartIcon className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div>
+            <div className="text-2xl font-light">Title</div>
+          </div>
+
+          <div className="flex justify-between">
+            <div className="text-md font-light">Post</div>
+            <div className="flex">
+              {post.published ? (
+                <p className="text-success self-end">Live</p>
+              ) : (
+                <p className="text-danger self-end">Unpublished</p>
+              )}
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      {/* New Design Option 3 */}
+      <div
+        className="p-3 my-4 mx-4 
+                  bg-blog-white 
+                  dark:bg-fun-blue-600 dark:text-blog-white
+                  rounded-lg
+                  drop-shadow-lg
+                  hover:drop-shadow-xl"
+      >
+        <div>
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <div className="text-3xl font-light">Title</div>
+              <div className="flex flex-col m-2">
+                <div className="font-extralight text-xs">9 Min Read</div>
+                <div className="font-extralight text-xs">45 Words</div>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="flex">
+                <div
+                  className="flex items-center justify-center
+                        bg-fun-blue-300 text-blog-black
+                        dark:text-blog-black w-[calc(4rem_*_0.5)] h-[calc(4rem_*_0.5)] p-1 m-0.5 rounded-full transition-filter duration-500 hover:filter hover:brightness-125
+                        "
+                >
+                  <div>{post.heartCount || 0}</div>
+                  <HeartIcon className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <div className="font-extralight text-xs">
+              {`${moment(post.createdAt).format("DD MMM YYYY hh:mm a")}`}
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <div>Post</div>
+            <div className="flex">
+              {post.published ? (
+                <p className="text-success self-end">Live</p>
+              ) : (
+                <p className="text-danger self-end">Unpublished</p>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
