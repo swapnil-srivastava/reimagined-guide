17ba35565c4d9f69cace50bc67c7ed536a57a573
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-15 16:32:51 +0200

    correcting the hear and remove heart labels

--- components/HeartButton.tsx
+++ components/HeartButton.tsx
@@ -61,7 +61,7 @@ export default function Heart({ postRef }) {
           <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
         </svg>
         <div className='ml-2 text-xs font-light'>
-          Heart 
+          Remove 
         </div>
       </button>
     </div>
@@ -78,7 +78,7 @@ export default function Heart({ postRef }) {
                 <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
               </svg>
           <div className='ml-2 text-xs font-light'>
-             Remove
+             Heart
           </div>
         </button>
       </div>
