07f9bcaafcd924f0933b28b3d6788ab28b2668e9
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-16 15:38:46 +0200

    allow auth domain to work

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,7 +11,7 @@ import "firebase/compat/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyARNiXCCoOl_sbTEy5j_TN3s-r8vnijWOg",
-  authDomain: "didactic-guide.firebaseapp.com",
+  authDomain: "swapnilsrivastava.eu",
   projectId: "didactic-guide",
   storageBucket: "didactic-guide.appspot.com",
   messagingSenderId: "571100157840",
