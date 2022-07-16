dd393ac0fc4944dffcd19cd7a6d308f752bbacb8
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-16 15:16:03 +0200

    adding back the authdomain

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,7 +11,7 @@ import "firebase/compat/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyARNiXCCoOl_sbTEy5j_TN3s-r8vnijWOg",
-  authDomain: "swapnilsrivastava.eu",
+  authDomain: "didactic-guide.firebaseapp.com",
   projectId: "didactic-guide",
   storageBucket: "didactic-guide.appspot.com",
   messagingSenderId: "571100157840",
