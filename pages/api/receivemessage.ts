1189327e3bc1e86406d819adfdf5860d5e33deeb
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-18 09:51:11 +0200

    changing the code

--- pages/api/receivemessage.ts
+++ pages/api/receivemessage.ts
@@ -1,9 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
+// import twilio from "twilio";
 
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 export default function receiveMessage(req: NextApiRequest, res: NextApiResponse) {
+    // const MessagingResponse = twilio.twiml.MessagingResponse;
     const twiml = new MessagingResponse();
 
     twiml.message('The Robots are coming! Head for the hills! - Swapnil Srivastava');
