235991ecf633826c283d1962536fe8c380af3400
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-19 18:30:40 +0200

    making it const

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import postmark from "postmark";
 
-var client = new postmark.ServerClient("2d92b8f0-fcc5-46e0-b368-98a4b9186755");
+const client = new postmark.ServerClient("2d92b8f0-fcc5-46e0-b368-98a4b9186755");
 
 export default function handler(req, res) {
-    
+
     client.sendEmail({
         "From": "contact@swapnilsrivastava.eu",
         "To": "contact@swapnilsrivastava.eu",
