0045d601d9039a189d3db0de92975816eb3acc97
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-20 12:41:54 +0200

    correctin the to and from and subject

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -6,11 +6,11 @@ const client = new postmark.ServerClient("2d92b8f0-fcc5-46e0-b368-98a4b9186755")
 export default function handler(req, res) {
 
     client.sendEmail({
-        "From": "contact@swapnilsrivastava.eu",
-        "To": "contact@swapnilsrivastava.eu",
-        "Subject": "Hello from Postmark",
-        "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
-        "TextBody": "Hello from Postmark!",
+        "From": "support@swapnilsrivastava.eu",
+        "To": "swapnilsrivastava68@hotmail.com",
+        "Subject": "Hello from Swapnil's Note",
+        "HtmlBody": "<strong>Hello</strong> dear Swapnil's Notes user, please visit https://swapnilsrivastava.eu for more info.",
+        "TextBody": "Hello from Swapnil's Notes!",
         "MessageStream": "outbound"
       });
 
