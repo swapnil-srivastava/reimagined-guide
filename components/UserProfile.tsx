cd380608903e4a410351ef738931ea285085d126
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-07-22 00:41:48 +0200

    open in another window

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -16,9 +16,9 @@ export default function UserProfile({user}) {
         <h1>{user.displayName || 'Anonymous User'}</h1>
         {(user.displayName === "Swapnil Srivastava") ? (
           <div className="flex items-center justify-center gap-x-2">
-            <a href="https://www.linkedin.com/in/swapnilsrivastava/"><FontAwesomeIcon icon={faLinkedin} size="2x" style={{ color: '#0072b1' }}/></a>
-            <a href="https://twitter.com/swapnil_sri"><FontAwesomeIcon icon={faTwitter} size="2x" style={{ color: '#00acee' }}/></a>
-            <a href="https://github.com/swapnil-srivastava"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
+            <a target="_blank" href="https://www.linkedin.com/in/swapnilsrivastava/" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" style={{ color: '#0072b1' }}/></a>
+            <a target="_blank" href="https://twitter.com/swapnil_sri" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} size="2x" style={{ color: '#00acee' }}/></a>
+            <a target="_blank" href="https://github.com/swapnil-srivastava" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
           </div>
         ) : <></> }
     </div>
