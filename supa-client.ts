5ef3ae6dfc16138aaf01c34902173822524123e0
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-01-17 23:31:22 +0100

    fix: adding the import of database interface for supabase client

--- supa-client.ts
+++ supa-client.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
 
 const supaBaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supaBaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supaClient = createClient(supaBaseUrl, supaBaseKey);
+export const supaClient = createClient<Database>(supaBaseUrl, supaBaseKey);
