fe8a118ded2c85517b49ff90c836260fba80c6f9
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-01-17 23:20:48 +0100

    fix: adding the policy for the user_profile

--- supabase/migrations/20230117215816_starting-ddl.sql
+++ supabase/migrations/20230117215816_starting-ddl.sql
@@ -6,3 +6,19 @@ create table user_profiles (
 );
 
 alter table user_profiles enable row level security
+
+CREATE POLICY "all can see" ON "public"."user_profiles"
+AS PERMISSIVE FOR SELECT
+TO public
+USING (true);
+
+CREATE POLICY "users can insert" ON "public"."user_profiles"
+AS PERMISSIVE FOR INSERT
+TO public
+WITH CHECK (auth.uid() = user_id);
+
+CREATE POLICY "owners can update" ON "public"."user_profiles"
+AS PERMISSIVE FOR UPDATE
+TO public
+USING (auth.uid()=user_id)
+WITH CHECK (auth.uid()=user_id);
