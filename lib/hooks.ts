1356e85d7f0fb7cd2ab9084a090601cf38c624b6
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-03-20 21:37:19 +0000

    adding changes for profile

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,7 +1,18 @@
 import { auth, firestore } from "../lib/firebase";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { RealtimeChannel, Session } from "@supabase/supabase-js";
 import { supaClient } from "../supa-client";
+
+export interface UserProfile {
+  username: string;
+  avatarUrl?: string;
+}
+
+export interface SupashipUserInfo {
+  session: Session | null;
+  profile: UserProfile | null;
+}
 
 // Custom hook to read  auth record and user profile doc
 export function useUserData() {
@@ -11,6 +22,7 @@ export function useUserData() {
     profile: null,
     session: null,
   });
+  const [channel, setChannel] = useState<RealtimeChannel | null>(null);
 
   useEffect(() => {
     supaClient.auth.getSession().then(({ data: { session } }) => {
@@ -22,6 +34,47 @@ export function useUserData() {
       });
     });
   }, []);
+
+  useEffect(() => {
+    if (userInfo.session?.user && !userInfo.profile) {
+      listenToUserProfileChanges(userInfo.session.user.id).then(
+        (newChannel) => {
+          if (channel) {
+            channel.unsubscribe();
+          }
+          setChannel(newChannel);
+        }
+      );
+    } else if (!userInfo.session?.user) {
+      channel?.unsubscribe();
+      setChannel(null);
+    }
+  }, [userInfo.session]);
+
+  async function listenToUserProfileChanges(userId: string) {
+    const { data } = await supaClient
+      .from("profiles")
+      .select("*")
+      .filter("id", "eq", userId);
+    if (data?.[0]) {
+      setUserInfo({ ...userInfo, profile: data?.[0] });
+    }
+    return supaClient
+      .channel(`public:profiles`)
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "profiles",
+          filter: `id=eq.${userId}`,
+        },
+        (payload) => {
+          setUserInfo({ ...userInfo, profile: payload.new as UserProfile });
+        }
+      )
+      .subscribe();
+  }
 
   useEffect(() => {
     // turn off realtime subscription
