a2adf4e2f70552b6a24372432ae402937e95a15d
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-03-22 10:17:55 +0000

    UserProfile and /username page

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -9,12 +9,12 @@ export default function UserProfile({user}) {
 
   return (
     <div className="flex flex-col items-center justify-center gap-2 dark:text-blog-white">
-        <img src={user.photoURL || '/hacker.png'} className="card-img-center" />
+        <img src={user.avatar_url || '/hacker.png'} className="card-img-center" />
             <p>
                 <i>@{user.username}</i>
             </p>
-        <h1>{user.displayName || 'Anonymous User'}</h1>
-        {(user.displayName === "Swapnil Srivastava") ? (
+        <h1>{user.full_name || 'Anonymous User'}</h1>
+        {(user.full_name === "Swapnil Srivastava") ? (
           <div className="flex items-center justify-center gap-x-2">
             <a target="_blank" href="https://www.linkedin.com/in/swapnilsrivastava/" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" style={{ color: '#0072b1' }}/></a>
             <a target="_blank" href="https://twitter.com/swapnil_sri" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} size="2x" style={{ color: '#00acee' }}/></a>
--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,49 +1,56 @@
-import React from 'react';
-import PostFeed from '../../components/PostFeed';
-import UserProfile from '../../components/UserProfile';
-import { getUserWithUsername, postToJSON } from '../../lib/firebase';
+import React from "react";
+import PostFeed from "../../components/PostFeed";
+import UserProfile from "../../components/UserProfile";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+import { getUserWithUsernameSupabase } from "./[slug]";
+import { supaClient } from "../../supa-client";
 
 // e.g. localhost:3000/swapnil
 // e.g. localhost:3000/ria
-export async function getServerSideProps({query}) {
+export async function getServerSideProps({ query }) {
   const { username } = query;
-
-  const userDoc = await getUserWithUsername(username);
+  const userPosts = await getUserWithUsernameSupabase(username);
 
   // If no user, short circuit to 404 page
-  if (!userDoc) {
+  if (!userPosts) {
     return {
       notFound: true,
     };
   }
-  
+
   // JSON serializable data
   let user = null;
   let posts = null;
 
-  if (userDoc) {
-    user = userDoc.data();
-    const postsQuery = userDoc.ref
-      .collection('posts')
-      .where('published', '==', true)
-      .orderBy('createdAt', 'desc')
+  if (userPosts) {
+    let { data: profiles } = await supaClient
+      .from("profiles")
+      .select("*")
+      .like("username", username);
 
-    posts = (await postsQuery.get()).docs.map(postToJSON);
-    
+    user = profiles;
+
+    let { data: supaPosts } = await supaClient
+      .from("posts")
+      .select("*")
+      .like("username", username)
+      .is("published", true);
+
+    posts = supaPosts;
   }
 
   return {
-    props: {user, posts}, // will be passed as props to the component
-  }
+    props: { user, posts }, // will be passed as props to the component
+  };
 }
 
-function UserProfilePage({user, posts}) {
+function UserProfilePage({ user, posts }) {
   return (
     <>
       <UserProfile user={user}></UserProfile>
       <PostFeed posts={posts} admin enableLoadMore={false}></PostFeed>
     </>
-  )
+  );
 }
 
-export default UserProfilePage
+export default UserProfilePage;
