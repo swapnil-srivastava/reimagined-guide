3f8deffbfb80c1cb6abd36e2f4ca905408e59c47
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-03-26 22:58:48 +0000

    adding the animated bounce for down arrow

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,9 @@ import Metatags from "../components/Metatags";
 import { supaClient } from "../supa-client";
 import { POST } from "../database.types";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
+
 // Max post to query per page
 const LIMIT = 5;
 
@@ -57,20 +60,6 @@ export default function Home(props) {
   return (
     <main>
       <Metatags />
-      {/* <div className="flex items-center p-3">
-        <blockquote className="lg:text-8xl text-5xl font-semibold italic text-center text-slate-900">
-          When you look
-          <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block mx-2">
-            <span className="relative text-white">annoyed</span>
-          </span>
-          all the time, people think that you're busy. -{" "}
-          <FormattedMessage
-            id="swapnil_name"
-            description="Name of the Author" // Description should be a string literal
-            defaultMessage="Swapnil Srivastava" // Message should be a string literal
-          />
-        </blockquote>
-      </div> */}
       <div className="p-3 flex justify-center items-center h-screen">
         <blockquote className="lg:text-8xl text-5xl font-semibold italic text-center text-slate-90 dark:text-blog-white text-blog-black">
           <FormattedMessage
@@ -105,6 +94,15 @@ export default function Home(props) {
             </span>
           </span>
         </blockquote>
+      </div>
+      <div className="relative">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <FontAwesomeIcon
+            className="animate-bounce h-15 w-15 mb-10 dark:text-blog-white text-fun-blue-500"
+            icon={faAnglesDown}
+            size={"3x"}
+          />
+        </div>
       </div>
       <div className="lg:flex lg:flex-row flex-wrap gap-x-4 ml-4 mr-4">
         <PostFeed
