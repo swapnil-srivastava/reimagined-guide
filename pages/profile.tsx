becca3376b938f9372eaef5fc89ef303a1b5fa92
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-03-27 20:03:00 +0000

    adding the profile with a card for modification later

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,12 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
+
 function Profile() {
   return (
     <>
-      <div>Swapnil Srivastava</div>
-      <div>Profile Picture</div>
-      <div>Email</div>
-      <div>Github</div>
-      <div>LinkedIn</div>
-      <div>Twitter</div>
+      <div
+        className="p-3 px-16 my-4 
+            bg-fun-blue-600
+            dark:bg-hit-pink-500 dark:text-blog-white
+            rounded-lg 
+            drop-shadow-lg
+            hover:drop-shadow-xl
+            flex items-center justify-center
+            hover:brightness-125"
+      >
+        <button
+          className="
+                focus:outline-none focus:ring-2 
+                focus:ring-fun-blue-400 
+                focus:ring-offset-2 text-sm 
+                text-blog-black
+                font-semibold 
+                h-12 px-3 rounded-lg
+                bg-hit-pink-500
+                dark:bg-fun-blue-600 dark:text-blog-white
+                transition-transform pointer-events-auto
+                transition-filter duration-500 hover:filter hover:brightness-125
+                flex items-center
+                "
+          onClick={() => console.log("Hello")}
+        >
+          <div className="pr-2">Load More</div>
+          <FontAwesomeIcon icon={faAnglesRight} size={"3x"} />
+        </button>
+      </div>
     </>
   );
 }
