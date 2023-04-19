4a0ecbb200e167fa156bc6852bc786fb395c5ca3
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-19 21:14:40 +0000

    adding the send the email

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -1,16 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const postmark = require("postmark");
 
+// Send an email:
 const client = new postmark.ServerClient(process.env.EMAIL_KEY);
 
 export default function handler(req, res) {
   client.sendEmail({
-    From: "contact@swapnilsrivastava.eu",
-    To: "contact@swapnilsrivastava.eu",
-    Subject: "Hello from Postmark",
-    HtmlBody: "<strong>Hello</strong> dear Postmark user.",
-    TextBody: "Hello from Postmark!",
-    MessageStream: "outbound",
+    "From": "contact@swapnilsrivastava.eu",
+    "To": "contact@swapnilsrivastava.eu",
+    "Subject": "Hello from Postmark",
+    "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
+    "TextBody": "Hello from Postmark!",
+    "MessageStream": "outbound"
   });
 
   res.status(200).json("Email had been sent");
