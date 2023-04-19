899d39854bfb8f0f86f3d869d5735c14f158cfd1
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-19 19:16:03 +0000

    renaming the email api key

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -1,19 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const postmark = require("postmark");
 
-const client = new postmark.ServerClient(process.env.NEXT_PUBLIC_EMAIL);
+const client = new postmark.ServerClient(process.env.EMAIL_KEY);
 
 export default function handler(req, res) {
+  client.sendEmail({
+    From: "support@swapnilsrivastava.eu",
+    To: "swapnilsrivastava68@hotmail.com",
+    Subject: "Hello from Swapnil's Note",
+    HtmlBody:
+      "<strong>Hello</strong> dear Swapnil's Notes user, please visit https://swapnilsrivastava.eu for more info.",
+    TextBody: "Hello from Swapnil's Notes!",
+    MessageStream: "outbound",
+  });
 
-    client.sendEmail({
-        "From": "support@swapnilsrivastava.eu",
-        "To": "swapnilsrivastava68@hotmail.com",
-        "Subject": "Hello from Swapnil's Note",
-        "HtmlBody": "<strong>Hello</strong> dear Swapnil's Notes user, please visit https://swapnilsrivastava.eu for more info.",
-        "TextBody": "Hello from Swapnil's Notes!",
-        "MessageStream": "outbound"
-      });
-
-    res.status(200).json("Email had been sent")
-  }
-  
+  res.status(200).json("Email had been sent");
+}
