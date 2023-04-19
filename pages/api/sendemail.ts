28526758ed01a798faba7d7f1f790e838e040352
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-19 20:35:32 +0000

    adding the outbound

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -4,15 +4,13 @@ const postmark = require("postmark");
 const client = new postmark.ServerClient(process.env.EMAIL_KEY);
 
 export default function handler(req, res) {
-  console.log("process.env.EMAIL_KEY", process.env.EMAIL_KEY);
-
   client.sendEmail({
-    "From": "contact@swapnilsrivastava.eu",
-    "To": "contact@swapnilsrivastava.eu",
-    "Subject": "Hello from Postmark",
-    "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
-    "TextBody": "Hello from Postmark!",
-    "MessageStream": "broadcast"
+    From: "contact@swapnilsrivastava.eu",
+    To: "contact@swapnilsrivastava.eu",
+    Subject: "Hello from Postmark",
+    HtmlBody: "<strong>Hello</strong> dear Postmark user.",
+    TextBody: "Hello from Postmark!",
+    MessageStream: "outbound",
   });
 
   res.status(200).json("Email had been sent");
