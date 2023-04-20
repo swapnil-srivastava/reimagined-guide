fcf9a50da6beb6ec46e3da686bc8f34d63984156
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-20 19:16:53 +0000

    adding the detials

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -2,16 +2,18 @@
 const postmark = require("postmark");
 
 // Send an email:
-const client = new postmark.ServerClient(process.env.EMAIL_KEY);
+const client = new postmark.ServerClient(
+  "2e7cd3fe-f589-4fa6-b0d5-2c1459e2af88"
+);
 
 export default function handler(req, res) {
   client.sendEmail({
-    "From": "contact@swapnilsrivastava.eu",
-    "To": "contact@swapnilsrivastava.eu",
-    "Subject": "Hello from Postmark",
-    "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
-    "TextBody": "Hello from Postmark!",
-    "MessageStream": "outbound"
+    From: "contact@swapnilsrivastava.eu",
+    To: "contact@swapnilsrivastava.eu",
+    Subject: "Hello from Postmark",
+    HtmlBody: "<strong>Hello</strong> dear Postmark user.",
+    TextBody: "Hello from Postmark!",
+    MessageStream: "outbound",
   });
 
   res.status(200).json("Email had been sent");
