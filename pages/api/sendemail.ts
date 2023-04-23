f0b2c2052c5e58492d7a57d44018b863565ee2cd
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-23 12:21:09 +0200

    adding the async and await for sendemail

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -1,20 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import * as postmark from "postmark";
+import { NextApiRequest, NextApiResponse } from "next";
+import postmark from "postmark";
 
-export default function handler(req, res) {
-  // Send an email:
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const client = new postmark.ServerClient(
-    "2e7cd3fe-f589-4fa6-b0d5-2c1459e2af88"
+    "2d92b8f0-fcc5-46e0-b368-98a4b9186755"
   );
 
-  client.sendEmail({
-    From: "contact@swapnilsrivastava.eu",
-    To: "contact@swapnilsrivastava.eu",
-    Subject: "Hello from nextjs api",
-    HtmlBody: "<strong>Hello</strong> dear Postmark user.",
-    TextBody: "Hello from Postmark!",
-    MessageStream: "outbound",
-  });
-
-  res.status(200).json("Email had been sent");
+  try {
+    const response = await client.sendEmail({
+      From: "support@swapnilsrivastava.eu",
+      To: "contact@swapnilsrivastava.eu",
+      Subject: "Hello from Swapnil's Note",
+      HtmlBody:
+        "<strong>Hello</strong> dear Swapnil's Notes user, please visit https://swapnilsrivastava.eu for more info.",
+      TextBody: "Hello from Swapnil's Notes!",
+      MessageStream: "outbound",
+    });
+    console.log(response);
+    res.status(200).json("Email had been sent");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to send email." });
+  }
 }
