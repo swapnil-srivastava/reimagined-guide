73fa847b7864544ddfe4a8725a1bc63dbfee9bb7
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-23 13:01:02 +0200

    making changes sendemail

--- pages/api/sendemail.ts
+++ pages/api/sendemail.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
-import postmark from "postmark";
+import * as postmark from "postmark";
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,7 +17,7 @@ export default async function handler(
       Subject: "Hello from Swapnil's Note",
       HtmlBody:
         "<strong>Hello</strong> dear Swapnil's Notes user, please visit https://swapnilsrivastava.eu for more info.",
-      TextBody: "Hello from Swapnil's Notes!",
+      TextBody: "Hello from Postmark!",
       MessageStream: "outbound",
     });
     res.status(200).json("Email had been sent");
