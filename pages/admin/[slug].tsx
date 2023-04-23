1a77a3209f89613edcb7d90133935cec9cc0e8a3
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-23 16:22:39 +0200

    check for the input length before

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -91,6 +91,7 @@ function PostManager() {
   }
 
   function generateMetaDescription(input) {
+    if (input.length === 0) return;
     if (input.length > 100) {
       return input.substring(0, 100) + "...";
     }
