3387cd980633fb8ad31b7b3164c5d47ae639efe0
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-30 17:14:23 +0200

    adding cover image

---
+++ pages/coverimage/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import mountains from "../../public/mountains.jpg";
+
+function Pics() {
+  return (
+    <>
+      <Image
+        alt="Mountains"
+        src={mountains}
+        placeholder="blur"
+        quality={100}
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+        }}
+      />
+    </>
+  );
+}
+
+export default Pics;
