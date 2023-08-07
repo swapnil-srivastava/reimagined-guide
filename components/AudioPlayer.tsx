956529398558279c19878c6639709d01ffd1d9d4
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-08-07 02:29:04 +0200

    fix: adding the functionality of mute, volume and play

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -7,20 +7,54 @@ import {
   faForward,
   faPause,
   faPlay,
+  faVolumeHigh,
+  faVolumeLow,
   faVolumeXmark,
 } from "@fortawesome/free-solid-svg-icons";
+import { Slider } from "@mui/material";
 
 import BasicTooltip from "./Tooltip";
 
 export default function AudioPlayer() {
-  const [isPlaying, setIsPlaying] = useState(false);
+  // state of play or pause
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  // state of duration
   const [duration, setDuration] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
+
+  // state of current time
+  const [currentTime, setCurrentTime] = useState<number>(0);
+
+  // State of volume
+  const [volumeLevel, setVolumeLevel] = useState<number>(0);
+
+  // Audio Player Ref
   const audioPlayer = useRef();
 
+  // State of mute
+  const [isMuted, setIsMuted] = useState<boolean>(false);
+
   useEffect(() => {
+    // Setting the duration of the audio file
     setDuration((audioPlayer?.current as HTMLAudioElement).duration);
-    return () => {};
+
+    // Setting the current time of the Audio file
+    setCurrentTime((audioPlayer?.current as HTMLAudioElement).currentTime);
+
+    // Setting the volume
+    setVolumeLevel((audioPlayer?.current as HTMLAudioElement).volume * 100);
+
+    (audioPlayer?.current as HTMLAudioElement)?.addEventListener(
+      "timeupdate",
+      handleTimeUpdate
+    );
+
+    return () => {
+      (audioPlayer?.current as HTMLAudioElement)?.removeEventListener(
+        "timeupdate",
+        handleTimeUpdate
+      );
+    };
   }, [
     (audioPlayer?.current as HTMLAudioElement)?.onloadedmetadata,
     (audioPlayer?.current as HTMLAudioElement)?.readyState,
@@ -35,49 +69,90 @@ export default function AudioPlayer() {
   }
 
   function togglePlayPause() {
-    const prevValue = isPlaying;
-    setIsPlaying(!prevValue);
-    if (prevValue) {
-      (audioPlayer.current as HTMLAudioElement).play();
-    } else {
-      (audioPlayer.current as HTMLAudioElement).pause();
+    if (audioPlayer.current) {
+      if (!isPlaying) {
+        const playPromise = (audioPlayer.current as HTMLAudioElement).play();
+        if (playPromise !== undefined) {
+          playPromise
+            .then(() => {
+              setIsPlaying(true);
+            })
+            .catch((error) => {
+              console.error("Playback error:", error);
+            });
+        }
+      } else {
+        (audioPlayer.current as HTMLAudioElement).pause();
+        setIsPlaying(false);
+      }
     }
   }
 
+  function handleVolumeChange(e: Event) {
+    setVolumeLevel(parseFloat((e.target as HTMLButtonElement).value));
+    if (audioPlayer && audioPlayer.current) {
+      (audioPlayer.current as HTMLAudioElement).volume =
+        parseFloat((e.target as HTMLButtonElement).value) / 100;
+    }
+  }
+
+  function handleTimeUpdate(e) {
+    if (audioPlayer && audioPlayer.current) {
+      setCurrentTime((audioPlayer?.current as HTMLAudioElement).currentTime);
+    }
+  }
+
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    if (audioPlayer && audioPlayer.current) {
+      const newTime = (newValue as number) * (duration / 100);
+      (audioPlayer.current as HTMLAudioElement).currentTime = newTime;
+      setCurrentTime(newTime);
+    }
+  };
+
+  const toggleMute = () => {
+    if (audioPlayer && audioPlayer.current) {
+      (audioPlayer.current as HTMLAudioElement).muted = !(
+        audioPlayer.current as HTMLAudioElement
+      ).muted;
+      setIsMuted(!isMuted);
+    }
+  };
+
   return (
     <>
-      <div className="flex flex-row justify-center items-center gap-1 dark:bg-slate-900 dark:text-white ring-1 ring-black/5 dark:ring-white/10 shadow-lg p-2 rounded-lg">
-        {/* Audio Player */}
-        <audio ref={audioPlayer} src="/tadaa.mp3">
-          Your browser does not support the audio element.
-        </audio>
+      <div className="flex flex-col dark:bg-slate-900 dark:text-white ring-1 ring-black/5 dark:ring-white/10 shadow-lg p-2 rounded-lg">
+        <div className="flex flex-row justify-center items-center gap-1 ">
+          {/* Audio Player */}
+          <audio ref={audioPlayer} src="/tadaa.mp3">
+            Your browser does not support the audio element.
+          </audio>
 
-        {/* Backwards 5 Seconds */}
-        <button className="flex flex-row items-center">
-          <BasicTooltip title={"Backwards 5 seconds"} placement="bottom">
-            <div className="bg-fun-blue-300 dark:text-blog-black w-[calc(3rem_*_0.5)] h-[calc(3rem_*_0.5)] rounded-full flex items-center justify-center transition-filter duration-500 hover:filter hover:brightness-125">
-              <div className="flex flex-row items-center">
-                <FontAwesomeIcon icon={faBackward} size="xs" />
+          {/* Backwards 5 Seconds */}
+          <button className="flex flex-row items-center">
+            <BasicTooltip title={"Backwards 5 seconds"} placement="bottom">
+              <div className="bg-fun-blue-300 dark:text-blog-black w-[calc(3rem_*_0.5)] h-[calc(3rem_*_0.5)] rounded-full flex items-center justify-center transition-filter duration-500 hover:filter hover:brightness-125">
+                <div className="flex flex-row items-center">
+                  <FontAwesomeIcon icon={faBackward} size="xs" />
+                </div>
               </div>
-            </div>
-          </BasicTooltip>
-        </button>
+            </BasicTooltip>
+          </button>
 
-        {/* Forward 5 Seconds */}
-        <button className="flex flex-row gap-1 items-center">
-          <BasicTooltip title={"Forward 5 seconds"} placement="bottom">
-            <div className="bg-fun-blue-300 dark:text-blog-black w-[calc(3rem_*_0.5)] h-[calc(3rem_*_0.5)] p-0.5 m-0.5 rounded-full flex items-center justify-center transition-filter duration-500 hover:filter hover:brightness-125">
-              <FontAwesomeIcon icon={faForward} size="xs" />
-            </div>
-          </BasicTooltip>
-        </button>
+          {/* Forward 5 Seconds */}
+          <button className="flex flex-row gap-1 items-center">
+            <BasicTooltip title={"Forward 5 seconds"} placement="bottom">
+              <div className="bg-fun-blue-300 dark:text-blog-black w-[calc(3rem_*_0.5)] h-[calc(3rem_*_0.5)] p-0.5 m-0.5 rounded-full flex items-center justify-center transition-filter duration-500 hover:filter hover:brightness-125">
+                <FontAwesomeIcon icon={faForward} size="xs" />
+              </div>
+            </BasicTooltip>
+          </button>
 
-        {/* Play or Pause */}
-        <button
-          className="flex flex-row gap-1 items-center"
-          onClick={() => togglePlayPause()}
-        >
-          <BasicTooltip title={"Play or Pause"} placement="bottom">
+          {/* Play or Pause */}
+          <button
+            className="flex flex-row gap-1 items-center"
+            onClick={() => togglePlayPause()}
+          >
             <div className="bg-fun-blue-300 dark:text-blog-black w-[calc(3rem_*_0.5)] h-[calc(3rem_*_0.5)] p-0.5 m-0.5 rounded-full flex items-center justify-center transition-filter duration-500 hover:filter hover:brightness-125">
               {isPlaying ? (
                 <FontAwesomeIcon icon={faPause} size="xs" />
@@ -85,31 +160,57 @@ export default function AudioPlayer() {
                 <FontAwesomeIcon icon={faPlay} size="xs" />
               )}
             </div>
-          </BasicTooltip>
-        </button>
-        <div className="flex flex-row items-center gap-1">
-          {/* Current time */}
-          <div className="text-sm">{calculateTime(currentTime)}</div>
-
-          {/* Progress Bar */}
-          <div className="flex flex-row items-center">
-            <input type="range" defaultValue={0}></input>
+          </button>
+          <div className="flex flex-row items-center gap-2">
+            {/* Current time */}
+            <div className="text-sm">{calculateTime(currentTime)}</div>
+            {/* Progress Bar */}
+            <div className="flex flex-row items-center w-20 ml-1">
+              <Slider
+                aria-label="Volume"
+                size="small"
+                value={(currentTime / duration) * 100}
+                onChange={(e, newValue) => handleSliderChange(e, newValue)}
+              />
+            </div>
+            {/* Duration */}
+            <div className="text-sm ml-1">
+              {duration && !isNaN(duration) && calculateTime(duration)}
+            </div>
           </div>
 
-          {/* Duration */}
-          <div className="text-sm">
-            {duration && !isNaN(duration) && calculateTime(duration)}
+          {/* Mute button */}
+          <button
+            className="flex flex-row gap-1 items-center"
+            onClick={() => toggleMute()}
+          >
+            <BasicTooltip
+              title={isMuted ? "Unmute" : "Mute"}
+              placement="bottom"
+            >
+              <div className="bg-fun-blue-300 dark:text-blog-black w-[calc(3rem_*_0.5)] h-[calc(3rem_*_0.5)] p-0.5 m-0.5 rounded-full flex items-center justify-center transition-filter duration-500 hover:filter hover:brightness-125">
+                {isMuted ? (
+                  <FontAwesomeIcon icon={faVolumeHigh} size="xs" />
+                ) : (
+                  <FontAwesomeIcon icon={faVolumeXmark} size="xs" />
+                )}
+              </div>
+            </BasicTooltip>
+          </button>
+        </div>
+        <div className="flex flex-row gap-1 items-center">
+          <div className="flex flex-row items-center mr-2">
+            <FontAwesomeIcon icon={faVolumeLow} size="xs" />
+          </div>
+          <Slider
+            aria-label="Volume"
+            value={volumeLevel}
+            onChange={(e) => handleVolumeChange(e)}
+          />
+          <div className="flex flex-row items-center ml-2">
+            <FontAwesomeIcon icon={faVolumeHigh} size="xs" />
           </div>
         </div>
-
-        {/* Mute button */}
-        <button className="flex flex-row gap-1 items-center">
-          <BasicTooltip title={"mute"} placement="bottom">
-            <div className="bg-fun-blue-300 dark:text-blog-black w-[calc(3rem_*_0.5)] h-[calc(3rem_*_0.5)] p-0.5 m-0.5 rounded-full flex items-center justify-center transition-filter duration-500 hover:filter hover:brightness-125">
-              <FontAwesomeIcon icon={faVolumeXmark} size="xs" />
-            </div>
-          </BasicTooltip>
-        </button>
       </div>
     </>
   );
