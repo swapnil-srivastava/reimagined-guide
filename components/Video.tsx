d00eb44d01370b19e5e103510696c8c767fc3c9a
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-08-14 23:38:42 +0200

    fix: adding the iframe to the video component

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Video = () => {
-  return <div>Video</div>;
+  return (
+    <>
+      <iframe
+        className="w-full aspect-video"
+        src="https://www.youtube.com/embed/VcnROkRhJ34?si=XWwW27TetPSNbSQg"
+        title="YouTube Video Player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </>
+  );
 };
 
 export default Video;
