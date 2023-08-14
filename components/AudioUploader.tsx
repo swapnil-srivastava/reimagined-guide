40522c961d3ad04914333844298bf8e470d7ae72
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-08-14 20:59:23 +0200

    fix: uploading all the three options for reference in the future

--- components/AudioUploader.tsx
+++ components/AudioUploader.tsx
@@ -72,14 +72,15 @@ export default function AudioUploader({ getAudioFileName }) {
   };
 
   return (
-    <div className="">
+    <div>
       <Loader show={uploading} />
       {uploading && <h3>{progress}</h3>}
 
       {!uploading && (
         <>
+          {/*         
           <label htmlFor="audio-upload" className="block p-1">
-            <span className="sr-only">Choose Audio</span>
+            Audio Upload <span className="sr-only">Choose Audio</span>
             <input
               type="file"
               id="audio-upload"
@@ -94,7 +95,72 @@ export default function AudioUploader({ getAudioFileName }) {
                 transition-filter duration-500 hover:filter hover:brightness-125
                 "
             />
+          </label> */}
+          Option 1
+          {/* <label
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="file_input"
+          >
+            Upload Audio
           </label>
+          <input
+            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            id="file_input"
+            type="file"
+          ></input>
+
+          <label
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="file_input1"
+          >
+            Upload file
+          </label> */}
+          {/* Option 2 */}
+          {/* <input
+            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            aria-describedby="file_input_help"
+            id="file_input1"
+            type="file"
+          />
+          <p
+            className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+            id="file_input_help"
+          >
+            SVG, PNG, JPG or GIF (MAX. 800x400px).
+          </p> */}
+          {/* Option 3  */}
+          <div className="flex items-center justify-center w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  SVG, PNG, JPG or GIF (MAX. 800x400px)
+                </p>
+              </div>
+              <input id="dropzone-file" type="file" className="hidden" />
+            </label>
+          </div>
         </>
       )}
     </div>
