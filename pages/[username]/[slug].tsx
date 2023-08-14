caa8a0070bbd9ba25f3c25e965403037cb915aca
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-08-14 16:26:51 +0200

    fix: adding the public url

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -91,14 +91,18 @@ function Post(props) {
     const [firstPost] = posts;
     setPost(firstPost);
 
-    // Get the audio URL of the post.
-    // Note Url only valid for 10 mins.
-    const { data: dataUrl, error: errorUrl } = await supaClient.storage
+    const { data: dataUrl } = supaClient.storage
       .from("audio")
-      .createSignedUrl(firstPost?.audio, 600); // Valid for 600 seconds = 10 mins
+      .getPublicUrl(firstPost?.audio);
 
-    const { signedUrl } = dataUrl;
-    setPostAudioUrl(signedUrl);
+    // Get the audio URL of the post.
+    // // Note Url only valid for 10 mins.
+    // const { data: dataUrl, error: errorUrl } = await supaClient.storage
+    //   .from("audio")
+    //   .createSignedUrl(firstPost?.audio, 600); // Valid for 600 seconds = 10 mins
+
+    const { publicUrl } = dataUrl;
+    setPostAudioUrl(publicUrl);
   };
 
   useEffect(() => {
