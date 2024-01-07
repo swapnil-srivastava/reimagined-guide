80306a037524f828a4dd12b3a8591ca9310c9bb2
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-01-07 17:37:59 +0000

    fix: adding the color to the icons using className for font

--- components/HorizontalScrollTech.tsx
+++ components/HorizontalScrollTech.tsx
@@ -7,21 +7,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const HorizontalScrollTech = ({ reverse = false }) => {
   return (
     <div className={`px-5 flex dark:text-blog-white flex-row justify-start items-center gap-8 ${reverse ? 'animate-infinite-scroll' : 'animate-infinite-reverse'}`}>
-      <FontAwesomeIcon icon={faJava} size="8x" />
-      <FontAwesomeIcon icon={faReact} size="8x"/>
-      <FontAwesomeIcon icon={faPython} size="8x" />
-      <FontAwesomeIcon icon={faAngular} size="8x" />
-      <FontAwesomeIcon icon={faNode} size="8x" />
-      <FontAwesomeIcon icon={faVuejs} size="8x" /> 
-      <FontAwesomeIcon icon={faJs} size="8x"/>
-      <FontAwesomeIcon icon={faCss3Alt} size="8x"/>
-      <FontAwesomeIcon icon={faSass} size="8x" />
-      <FontAwesomeIcon icon={faHtml5} size="8x" />
-      <FontAwesomeIcon icon={faBootstrap} size="8x" /> 
+      <FontAwesomeIcon icon={faJava} size="8x" className={'text-[#5986a4]'}/>
+      <FontAwesomeIcon icon={faReact} size="8x" className={'text-[#149eca]'}/>
+      <FontAwesomeIcon icon={faPython} size="8x" className={'text-[#427aa9]'}/>
+      <FontAwesomeIcon icon={faAngular} size="8x" className={'text-[#c3012f]'}/>
+      <FontAwesomeIcon icon={faNode} size="8x" className={'text-[#85cf36]'}/>
+      <FontAwesomeIcon icon={faVuejs} size="8x" className={"text-[#347a59]"} /> 
+      <FontAwesomeIcon icon={faJs} size="8x" className={'text-[#f4dd58]'}/>
+      <FontAwesomeIcon icon={faCss3Alt} size="8x" className={'text-[#2d53e5]'}/>
+      <FontAwesomeIcon icon={faSass} size="8x" className={'text-[#cf6c9c]'}/>
+      <FontAwesomeIcon icon={faHtml5} size="8x" className={'text-[#e5532f]'}/>
+      <FontAwesomeIcon icon={faBootstrap} size="8x" className={'text-[#781bf8]'}/> 
       <FontAwesomeIcon icon={faGithub} size="8x" />
-      <FontAwesomeIcon icon={faYarn} size="8x" /> 
-      <FontAwesomeIcon icon={faNpm} size="8x" />
-      <FontAwesomeIcon icon={faGit} size="8x" />
+      <FontAwesomeIcon icon={faYarn} size="8x" className={'text-[#3592bd]'}/> 
+      <FontAwesomeIcon icon={faNpm} size="8x" className={'text-[#c3292f]'}/>
+      <FontAwesomeIcon icon={faGit} size="8x" className={'text-[#f1563b]'}/>
     </div>
   );
 };
