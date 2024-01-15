cf2bd695990cf9a79b2adfad196e4c0c55fdb23d
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-01-15 01:35:24 +0000

    fix: adding the text color white for dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -165,7 +165,7 @@ export default function Home(props) {
         </div>
       </div>
       
-      <div className="font-poppins text-5xl flex justify-center">Unveiling the Secrets: Dive into our Latest Article</div>
+      <div className="font-poppins text-5xl flex justify-center dark:text-blog-white">Unveiling the Secrets: Dive into our Latest Article</div>
 
       {/* Post Feed  */}
       <div className={`h-screen flex justify-center ${posts?.length < 3 ? "" : "lg:ml-96"}`}>
