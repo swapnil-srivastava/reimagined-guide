e7b04bcf6f616889c87e97d57c666d2e6dcf36f7
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-01-18 03:36:25 +0000

    fix: adding the details for pricing plan

--- pages/pricing/index.tsx
+++ pages/pricing/index.tsx
@@ -5,18 +5,18 @@ import RoundButton from "../../components/RoundButton";
 
 const Pricing = () => {
   return (
-    <div className="h-screen flex flex-col items-center gap-2 justify-center font-poppins">
+    <div className="h-screen flex flex-col items-center gap-2 font-poppins mt-10">
         {/* Title */}
-        <div className="text-5xl  dark:text-blog-white">Choose the right plan which is best for You!</div>
+        <div className="text-5xl dark:text-blog-white text-center">Choose the right plan which is best for You!</div>
 
         {/* Pricing cards */}
         <div className="flex lg:flex-row flex-col gap-5 m-10">
             {/* Pricing card 1 */}
             <div className="mx-10 p-10 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
-              <div className="text-center font-thin">FREE</div>
-              <div className="text-4xl text-center">$ 0</div>
-              <div className="text-center font-thin">No Payment</div>
-              <div className="flex flex-col">
+              <div className="text-center font-thin leading-loose">FREE</div>
+              <div className="text-4xl text-center leading-loose">$ 0</div>
+              <div className="text-center font-thin leading-loose">No Payment</div>
+              <div className="flex flex-col leading-loose">
                 <div className="flex gap-2">
                   <div><FontAwesomeIcon icon={faCheck} /></div>
                   <div>Things to be mentioned</div>
@@ -40,7 +40,7 @@ const Pricing = () => {
               </div>
               
               {/* button section */}
-              <div className="text-center font-thin flex items-center justify-center">
+              <div className="text-center font-thin flex items-center justify-center mt-3">
                 <button
                   type="button"
                   className="bg-hit-pink-500 text-blog-black
@@ -58,11 +58,11 @@ const Pricing = () => {
 
             {/* Pricing card 2 */}
             <div className="mx-10 p-10 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
-              <div className="text-center font-thin">FREE</div>
-              <div className="text-4xl text-center">$ 10</div>
-              <div className="text-center font-thin">Payment</div>
-              <div className="flex flex-col leading-relaxed">
-                <div className="flex gap-2">
+              <div className="text-center font-thin leading-loose">Beginner</div>
+              <div className="text-4xl text-center leading-loose">$ 10</div>
+              <div className="text-center font-thin leading-loose">Payment</div>
+              <div className="flex flex-col leading-loose">
+                <div className="flex gap-2 leading-loose">
                   <div><FontAwesomeIcon icon={faCheck} /></div>
                   <div>Things to be mentioned</div>
                 </div>
@@ -85,7 +85,7 @@ const Pricing = () => {
               </div>
               
               {/* button section */}
-              <div className="text-center font-thin flex items-center justify-center">
+              <div className="text-center font-thin flex items-center justify-center mt-3">
                 <button
                   type="button"
                   className="bg-hit-pink-500 text-blog-black
@@ -103,10 +103,10 @@ const Pricing = () => {
 
             {/* Pricing card 3 */}
             <div className="mx-10 p-10 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
-              <div className="text-center font-thin">FREE</div>
-              <div className="text-4xl text-center">$ 100</div>
-              <div className="text-center font-thin">Payment</div>
-              <div className="flex flex-col">
+              <div className="text-center font-thin leading-loose">Expert</div>
+              <div className="text-4xl text-center leading-loose">$ 100</div>
+              <div className="text-center font-thin leading-loose">Payment</div>
+              <div className="flex flex-col leading-loose">
                 <div className="flex gap-2">
                   <div><FontAwesomeIcon icon={faCheck} /></div>
                   <div>Things to be mentioned</div>
@@ -130,7 +130,7 @@ const Pricing = () => {
               </div>
               
               {/* button section */}
-              <div className="text-center font-thin flex items-center justify-center">
+              <div className="text-center font-thin flex items-center justify-center mt-3">
                 <button
                   type="button"
                   className="bg-hit-pink-500 text-blog-black
