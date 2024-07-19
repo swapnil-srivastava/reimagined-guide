f59e5bb273662996a66fca3e2a7838fd37ff703a
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-19 01:00:59 +0000

    adding the base configuration for stripe

--- pages/api/checkout/route.ts
+++ pages/api/checkout/route.ts
@@ -3,9 +3,35 @@ import { stripe } from '../../../lib/stripe/stripe';
 
 export async function POST(request: Request) { 
     try {
+        const { priceId, email, userId } = await request.json();
 
+        const session = await stripe.checkout.sessions.create({
+            metadata: {
+                user_id: userId,
+            },
+            customer_email: email,
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    // base subscription
+                    price: priceId,
+                },
+                {
+                    // one-time setup fee
+                    price: 'price_1OtHdOBF7AptWZlcPmLotZgW',
+                    quantity: 1,
+                },
+            ],
+            mode: 'subscription',
+            success_url: `${request.headers.get('origin')}/success`,
+            cancel_url: `${request.headers.get('origin')}/cancel`,
+        });
+
+        return NextResponse.json({ id: session.id });
+        
     } catch( error: any ) {
-
+        console.error(error);
+        return NextResponse.json({ message: error.message }, { status: 500 });
     }
 }
 
