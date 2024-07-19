d48a9742dd930476e531ef4d7936060381824c37
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-19 00:51:14 +0000

    add the stripe node

---
+++ pages/api/checkout/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import { stripe } from '../../../lib/stripe/stripe';
+
+export async function POST(request: Request) { 
+    try {
+
+    } catch( error: any ) {
+
+    }
+}
+
