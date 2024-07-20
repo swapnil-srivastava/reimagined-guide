e56fe35d9fb3255db67b8940dbe39fb0590b993d
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-20 21:54:10 +0000

    adding the logic for call the api

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,6 +1,39 @@
 'use client';
 
+import { loadStripe } from '@stripe/stripe-js';
+import toast from 'react-hot-toast';
+
+// supabase instance in the app
+import { supaClient } from "../supa-client";
+
 const CheckoutButton = ({  }) => {
+    const handleCheckout = async() => {
+        const { data } = await supaClient.auth.getUser();
+    
+        if (!data?.user) {
+          toast.error("Please log in to create a new Stripe Checkout session");
+          return;
+        }
+    
+        const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+        const stripe = await stripePromise;
+
+        const response = await fetch('/api/checkout', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ priceId: 'price_1OtHkdBF7AptWZlcIjbBpS8r', userId: data.user?.id, email: data.user?.email }),
+          });
+        
+        const session = await response.json();
+
+        await stripe?.redirectToCheckout({ sessionId: session.id });
+
+      }
+
+
     return (
       <>
         <button
@@ -12,6 +45,7 @@ const CheckoutButton = ({  }) => {
             focus:ring-fun-blue-400 
             focus:ring-offset-2
             dark:text-blog-black"
+            onClick={() => handleCheckout()}
         >
             {"Let\'s get started"}
         </button>
