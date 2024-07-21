92a85a0fc5abe4a4814793721d05466ade1f71a8
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-21 00:20:58 +0000

    replacing the method

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -9,45 +9,65 @@ import { supaClient } from "../supa-client";
 
 const CheckoutButton = ({  }) => {
     const handleCheckout = async() => {
-        const { data: supabaseData } = await supaClient.auth.getUser();
+        // const { data: supabaseData } = await supaClient.auth.getUser();
     
-        if (!supabaseData?.user) {
+        // if (!supabaseData?.user) {
+        //   toast.error("Please log in to create a new Stripe Checkout session");
+        //   return;
+        // }
+    
+        // console.log("called the supabase");
+
+        // const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+        // const stripe = await stripePromise;
+
+        // console.log("called the stripe");
+
+        // // const { data, status } = await axios.post(
+        // //     "/api/checkout",
+        // //     { priceId: 'price_1Pe47VRomQdDoc7IzPHbnYkn', userId: supabaseData.user?.id, email: supabaseData.user?.email },
+        // //     {
+        // //       headers: {
+        // //         "Content-Type": "application/json",
+        // //       },
+        // //     }
+        // //   );
+
+        // // console.log("data ===> stripe", data);
+    
+        // // await stripe?.redirectToCheckout({ sessionId: data.id });
+
+        // const response = await fetch('/api/checkout', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify({ priceId: 'price_1Pe47VRomQdDoc7IzPHbnYkn', userId: supabaseData.user?.id, email: supabaseData.user?.email }),
+        // });
+
+        // const session = await response.json();
+        // await stripe?.redirectToCheckout({ sessionId: session.id });
+
+        const { data } = await supaClient.auth.getUser();
+
+        if (!data?.user) {
           toast.error("Please log in to create a new Stripe Checkout session");
           return;
         }
     
-        console.log("called the supabase");
-
         const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-
         const stripe = await stripePromise;
-
-        console.log("called the stripe");
-
-        // const { data, status } = await axios.post(
-        //     "/api/checkout",
-        //     { priceId: 'price_1Pe47VRomQdDoc7IzPHbnYkn', userId: supabaseData.user?.id, email: supabaseData.user?.email },
-        //     {
-        //       headers: {
-        //         "Content-Type": "application/json",
-        //       },
-        //     }
-        //   );
-
-        // console.log("data ===> stripe", data);
-    
-        // await stripe?.redirectToCheckout({ sessionId: data.id });
-
         const response = await fetch('/api/checkout', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ priceId: 'price_1Pe47VRomQdDoc7IzPHbnYkn', userId: supabaseData.user?.id, email: supabaseData.user?.email }),
-        });
-
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ priceId: 'price_1Pe47VRomQdDoc7IzPHbnYkn', userId: data.user?.id, email: data.user?.email }),
+          });
         const session = await response.json();
         await stripe?.redirectToCheckout({ sessionId: session.id });
+    
 
       }
 
