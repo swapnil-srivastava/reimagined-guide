9e9c529ed9673133d495e0fd29a7f44057e024a2
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-25 22:56:21 +0000

    adding the font awesome heart icon

--- components/HeartButton.tsx
+++ components/HeartButton.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import { faHeart, faPenToSquare, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BasicTooltip from "./Tooltip";
 
 // Allows user to heart or like a post
@@ -37,20 +38,7 @@ export default function Heart({ postRef }) {
                 font-semibold"
           onClick={removeHeart}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 stroke-1 hover:stroke-2"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            />
-          </svg>
+          <FontAwesomeIcon icon={faHeart} />
           <div className="ml-2 text-sm md:text-md font-light">
             <FormattedMessage id="heart-button-remove"
               description="text on heart button to remove" // Description should be a string literal
@@ -73,18 +61,8 @@ export default function Heart({ postRef }) {
                           font-semibold"
           onClick={addHeart}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 stroke-1 hover:stroke-2"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-              clipRule="evenodd"
-            />
-          </svg>
+          {/* for adding heart */}
+          <FontAwesomeIcon icon={faHeart} /> 
           <div className="ml-2 text-sm md:text-md font-light">
             <FormattedMessage id="heart-button-heart"
               description="text on heart button to heart" // Description should be a string literal
