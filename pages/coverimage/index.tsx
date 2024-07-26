4a4b97dc7848f09043f83b469a1ad857f4a984ae
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 05:33:22 +0000

    addomg the email button from nestjs

--- pages/coverimage/index.tsx
+++ pages/coverimage/index.tsx
@@ -3,9 +3,16 @@ import Image from "next/legacy/image";
 import mountains from "../../public/mountains.jpg";
 import { Button } from "@mui/material";
 import { callNest } from "../../services/helloWorld.service";
-
+import { callNestSendEmail } from "../../services/email.service";
+import * as postmark from "postmark";
 
 function Pics() {
+  const emailMessage: Partial<postmark.Message> = {
+    To: "contact@swapnilsrivastava.eu",
+    Subject: "nest email",
+    HtmlBody: `<strong>Hello</strong> Swapnil Srivastava, nest email check`,
+  };
+
   return (
     <>
       <Image
@@ -19,6 +26,7 @@ function Pics() {
         }}
       />
       <Button onClick={() => callNest()}>call nest</Button>
+      <Button onClick={() => callNestSendEmail(emailMessage)}>Call send email from nest</Button>
     </>
   );
 }
