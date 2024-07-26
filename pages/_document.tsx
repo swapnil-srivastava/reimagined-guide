a2faef5aec5c4591a00bb0ddb5e634763ee2de63
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 23:07:04 +0000

    adding link in head section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <link rel="icon" href="https://dbydvpdhbaqudqqjteoq.supabase.co/storage/v1/object/sign/avatars/profile.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzL3Byb2ZpbGUucG5nIiwiaWF0IjoxNzIyMDMzMzMwLCJleHAiOjIwMzczOTMzMzB9.2fCp8-hDw_e05QacUp-MRSDYVp08Z-4TJzJ8RJqmyKo" type="image/x-icon" />
+            </Head>
             <body className="bg-blog-white dark:bg-fun-blue-500">
                 <Main />
                 <NextScript />
