31f83339bf3b8efc64aa77c3462d9168a1c63bbf
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-08-09 00:34:40 +0000

    adding a product to the cart display a card to redirect to card

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 import { FormattedMessage } from "react-intl";
+import { useSelector } from "react-redux";
+import Link from "next/link";
+
+// CSS
+import styles from "../../styles/Admin.module.css";
+
+// Components
 import ProductCard from "../../components/ProductCard";
 
+// Redux
+import { RootState } from "../../lib/interfaces/interface";
+
+
 function Products() {
+
+  const selectUser = (state: RootState) => state.cart;
+  const { cartItems } = useSelector(selectUser);
 
   const [products, setProducts] = useState(
     [
@@ -154,6 +168,29 @@ function Products() {
             />
         </div>
         <div className="lg:px-14 lg:pl-20 lg:py-5 px-2 py-1 flex flex-wrap flex-row lg:gap-x-20 lg:gap-y-11 gap-y-10 md:justify-start justify-center">
+
+          {/* if products have been added to the cart */}
+          {cartItems && cartItems.length > 0 &&
+              <div className="flex h-full w-full p-4 hover:px-5 lg:mx-0 mx-3 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
+                <div className="flex flex-col items-center gap-2 justify-center h-full w-full">
+                    <div className="text-center">
+                        <FormattedMessage
+                            id="product-index-cart-added"
+                            description="Product added to the cart" // Description should be a string literal
+                            defaultMessage="Product added to the cart" // Message should be a string literal
+                        />
+                    </div>
+                    <Link href="/cart" className="flex justify-center pt-3">
+                            <button className={styles.btnAdmin}>
+                                <FormattedMessage id="product-index-cart-added-redirect-btn"
+                                description="Go to cart" // Description should be a string literal
+                                defaultMessage="Go to cart" // Message should be a string literal
+                                />
+                            </button>
+                    </Link>
+                </div>
+            </div>
+          }
           <ProductCard products={products} loading={false} postsEnd={false} enableLoadMore={true} onQuantityChange={(product) => handleProductChange(product)}/>
         </div>
       </div>
