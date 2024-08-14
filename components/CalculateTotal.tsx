81a666204967e46c86afad08ba200bc8c4c1173c
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-08-14 22:53:50 +0000

    addign the flex

--- components/CalculateTotal.tsx
+++ components/CalculateTotal.tsx
@@ -31,7 +31,7 @@ const CalculateTotal : React.FC<PriceBreakdownCardProps> = ({ products, delivery
             <div className="flex h-full w-full p-4 hover:px-5 lg:mx-0 mx-3 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
                 <div className="flex flex-col w-full h-full gap-2 justify-center items-center">
                     <div className="flex flex-col items-center gap-2 justify-center h-full w-full">
-                        <div className="max-w-md mx-auto overflow-hidden h-full w-full flex">
+                        <div className="overflow-hidden h-full w-full flex flex-col">
                             <div>
                                 <div className="flex justify-between mb-2">
                                     <span className="text-gray-700">
