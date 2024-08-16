ff113c86d9acc02aeb9361b03edda293cc87f53e
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-08-16 10:28:21 +0000

    adding the text color at the div level and removing from the individual

--- components/CalculateTotal.tsx
+++ components/CalculateTotal.tsx
@@ -29,50 +29,50 @@ const CalculateTotal : React.FC<PriceBreakdownCardProps> = ({ products, delivery
     return (
       <>
         <div className="flex h-full w-full font-poppins">
-            <div className="flex h-full w-full p-4 hover:px-5 lg:mx-0 mx-3 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
+            <div className="flex h-full w-full p-4 hover:px-5 lg:mx-0 mx-3 bg-blog-white dark:bg-fun-blue-600  dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
                 <div className="flex flex-col w-full h-full gap-2 justify-center items-center">
                     <div className="flex flex-col items-center gap-2 justify-center h-full w-full">
                         <div className="overflow-hidden h-full w-full flex flex-col">
                             <div>
                                 <div className="flex justify-between mb-2">
-                                    <span className="text-gray-700">
+                                    <span>
                                         <FormattedMessage
                                             id="calculate-total-subtotal-text"
                                             description="Subtotal:" // Description should be a string literal
                                             defaultMessage="Subtotal:" // Message should be a string literal
                                         />
                                     </span>
-                                    <span className="text-gray-900">€ {subtotal.toFixed(2)}</span>
+                                    <span>€ {subtotal.toFixed(2)}</span>
                                 </div>
                                 <div className="flex justify-between mb-2">
-                                    <span className="text-gray-700">
+                                    <span>
                                         <FormattedMessage
                                             id="calculate-total-tax-text"
                                             description="Tax (19%):" // Description should be a string literal
                                             defaultMessage="Tax (19%):" // Message should be a string literal
                                         />
                                     </span>
-                                    <span className="text-gray-900">€ {tax.toFixed(2)}</span>
+                                    <span>€ {tax.toFixed(2)}</span>
                                 </div>
                                 <div className="flex justify-between mb-2">
-                                    <span className="text-gray-700">
+                                    <span>
                                         <FormattedMessage
                                             id="calculate-total-delivery-cost-text"
                                             description="Delivery Cost:" // Description should be a string literal
                                             defaultMessage="Delivery Cost:" // Message should be a string literal
                                         />
                                     </span>
-                                    <span className="text-gray-900">€ {deliveryCost.toFixed(2)}</span>
+                                    <span>€ {deliveryCost.toFixed(2)}</span>
                                 </div>
                                 <div className="flex justify-between font-bold">
-                                    <span className="text-gray-700">
+                                    <span>
                                         <FormattedMessage
                                             id="calculate-total-text"
                                             description="Total:" // Description should be a string literal
                                             defaultMessage="Total:" // Message should be a string literal
                                         />
                                     </span>
-                                    <span className="text-gray-900">€ {total.toFixed(2)}</span>
+                                    <span>€ {total.toFixed(2)}</span>
                                 </div>
                             </div>
                         </div>
