964e4f3bd342f560fbb38e3a5521ee1833dc4dd3
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-08-18 22:55:17 +0000

    adding address update and delete

--- redux/reducer/addressReducer.ts
+++ redux/reducer/addressReducer.ts
@@ -16,7 +16,23 @@ export const addressReducer = (state = initialStoreState, { type, address }) => 
         }
       };
       return newState;
-  }
+  };
+  case types.ADD_TO_CART_ADDRESS_UPDATE: {
+    const newState = {
+      ...state,
+      customerAddress : {
+        ...address
+      }
+    };
+    return newState;
+  };
+  case types.ADD_TO_CART_ADDRESS_DELETE: {
+    const newState = {
+      ...state,
+      customerAddress : { }
+    };
+    return newState;
+  };
     default:
       return state
   }
