da976c0a570a6237de4f1950eaa26507dbf34437
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-01 12:08:07 +0000

    adding the invite page

---
+++ pages/invite/index.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React, { useEffect, useState } from "react";
+
+function Invite() {
+  return (
+    <>
+        <div>
+            Hello Invite Page
+        </div>
+    </>
+  )
+}
+
+export default Invite;
