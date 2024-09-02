3845da56b20a2bde1a18defb2785d4a9b0bb56b2
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-02 00:48:40 +0000

    adding the message for invite list

--- pages/invite/index.tsx
+++ pages/invite/index.tsx
@@ -13,6 +13,9 @@ import { supaClient } from "../../supa-client";
 import { RootState } from "../../lib/interfaces/interface";
 import { fetchInviteEvents } from "../../redux/actions/actions";
 
+// CSS
+import styles from "../../styles/Admin.module.css";
+
 function Invite() {
   const dispatch = useDispatch();
   
@@ -44,7 +47,7 @@ function Invite() {
 
   return (
       <>
-        <div className="flex justify-start w-full lg:px-12 px-10 pb-3 font-poppins dark:text-blog-white lg:text-2xl text-lg">
+        <div className="flex justify-start w-full lg:px-12 px-10 pb-3 mt-7 font-poppins dark:text-blog-white lg:text-2xl text-lg">
             <FormattedMessage
                 id="invite-page-event-heading"
                 description="Event Details" // Description should be a string literal
@@ -116,40 +119,17 @@ function Invite() {
           <div className="flex h-full w-full p-4 hover:px-5 lg:mx-0 mx-3 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
               <div className="flex flex-col w-full h-full gap-2 justify-center items-center">
                   {/* Invited Family Row */}
-                  {/* {inviteEvents && inviteEvents.map((inviteEvent, index, array) => (
-                      <div key={inviteEvent.id} className="flex h-full w-full p-4 hover:px-5 lg:mx-0 mx-3 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl">
-                          <div className="flex flex-row gap-2 h-full w-full">
-                              <div className="flex">
-                                  <Image 
-                                    src={inviteEvent.image_url ?? `/mountains.jpg`} 
-                                    alt={inviteEvent.title}
-                                    width={250}
-                                    height={250}
-                                    className="rounded-lg"
-                                  />
-                              </div>
-                              <div className="flex flex-col w-full justify-between">
-                                  <div className="flex justify-between items-start">
-                                      <div>
-                                          <div className="lg:text-xl text-xs">{inviteEvent.title}</div>
-                                          <div className="lg:text-xl text-xs">{inviteEvent.description}</div>
-                                          <div className="lg:text-xl text-xs">{inviteEvent.location}</div>
-                                          <div className="lg:text-xl text-xs">{inviteEvent.time}</div>
-                                      </div>
-                                      <FontAwesomeIcon icon={faCircleXmark} className="cursor-pointer" size="xl" />
-                                  </div>
-                                  <div className="flex flex-row justify-between items-center">
-                                      <div>
-                                          
-                                      </div>
-                                      <div className="lg:text-2xl text-xs">
-                                          
-                                      </div>
-                                  </div>
-                              </div>
+                  <div className="flex h-full w-full p-4 hover:px-5 lg:mx-0 mx-3 bg-blog-white dark:bg-fun-blue-600 dark:text-blog-white hover:rounded-3xl rounded-3xl drop-shadow-lg hover:drop-shadow-xl hover:brightness-125">
+                    <div className="flex flex-col items-center gap-2 justify-center h-full w-full">
+                          <div className="text-center">
+                              <FormattedMessage
+                                  id="invite-page-invite-text"
+                                  description="Functionality comming soon" // Description should be a string literal
+                                  defaultMessage="Functionality coming soon" // Message should be a string literal
+                              />
                           </div>
-                      </div>
-                  ))} */}
+                    </div>
+                  </div>
               </div>
           </div>
         </div>
