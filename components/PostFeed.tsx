444d46998227c0c2e2c2186ce0133b10ca94b953
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2025-08-18 00:59:08 +0000

    internationalization

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -12,7 +12,7 @@ export function PostFeed({
   posts,
   user = undefined,
   admin = false,
-  parentFunction,
+  parentFunction = undefined,
   loading = false,
   postsEnd = false,
   enableLoadMore = false,
